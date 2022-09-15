d3d9e82a79e9ea13be937669356d5400ecee7336
DavidVal6 <98176834+DavidVal6@users.noreply.github.com>
2022-09-14 22:01:09 -0500

    Bluej Proyect

---
+++ Csa/ICPC.java
@@ -0,0 +1,370 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Comparator;
+import javax.swing.JOptionPane;
+import java.lang.Math;
+import java.awt.*;
+
+/**
+ * This class is a graphical representation of the ICPC problem B: The Cost of speed Limits
+ * 
+ * @author Olaya - Valencia
+ * @1.0
+ */
+public class ICPC
+{    
+    public static int length;
+    public static int width;
+    public static boolean isVisible = true;
+    private ArrayList<String> intersectionC = new ArrayList();
+    private HashMap<String, int[]> intersectionP = new HashMap();
+    private HashMap<String, Circle> intersectionO = new HashMap();
+    private ArrayList<String[]> routesC = new ArrayList();
+    private HashMap<String[], Rectangle> routesO = new HashMap();
+    private ArrayList<String[]> signsC = new ArrayList();
+    private HashMap<String[], Sign> signsO = new HashMap();
+    private boolean ok;
+    
+    /**
+     * Create a simulation of a ICPC network
+     * @param width  the desired width for the canvas
+     * @param height  the desired height for the canvas 
+     */
+    public ICPC(int length, int width){
+        this.width = width;
+        this.length = length;
+        if(isVisible){
+            Canvas canvas = Canvas.getCanvas(length, width);
+            canvas.wait(10);
+        }
+    }
+    
+    /**
+     * Add the new intersection to the canvas
+     * @param color the desired color and identification of the intersection (should be unique)
+     * @x y the desired position of the intersecion in the canvas
+     */
+    public void addIntersection(String color, int x, int y){
+        if(!intersectionC.contains(color)){
+            intersectionC.add(color);
+            int[] position = new int[2];
+            position[0] = x;
+            position[1] = y;
+            intersectionP.put(color, position);
+            createIntersection(color, x, y);
+            intersectionC.sort(Comparator.naturalOrder());
+            ok = true;
+        }else{
+            JOptionPane.showMessageDialog(null, "This intersection already exists");
+            ok = false;
+        }
+    }
+    
+    /**
+     * Create the intersection in the canvas
+     */
+    private void createIntersection(String color, int x, int y){
+        Circle circle = new Circle();
+        circle.moveHorizontal(x);
+        circle.moveVertical(y);
+        circle.changeColor(color);
+        intersectionO.put(color, circle);
+        circle.makeVisible();
+    }
+    
+    /**
+     * Add the new road to the canvas
+     * @param intersectionA and intersectionB indicates the begin and the end of the route 
+     */
+    public void addRoute(String intersectionA, String intersectionB){
+        String[] touple = new String[2];
+        touple[0] = intersectionA;
+        touple[1] = intersectionB;
+        String[] toupleR = reverseTouple(touple);
+        if(isIn(routesC, touple, toupleR) == false){
+            routesC.add(touple);
+            createRoute(intersectionA, intersectionB, touple);
+            ok = true;
+        }else{
+            JOptionPane.showMessageDialog(null, "This road already exists");
+            ok = false;
+        }
+    }
+    
+    /**
+     * Create the route in the canvas
+     */
+    private void createRoute(String intersectionA, String intersectionB, String[] touple){
+        Rectangle rect = new Rectangle();
+        rect.changeColor("black");
+        rect.moveHorizontal(intersectionP.get(intersectionA)[0]);
+        rect.moveVertical(intersectionP.get(intersectionA)[1]);
+        routesO.put(touple, rect);
+        resize(rect, intersectionA, intersectionB);
+        rotate(rect, intersectionA, intersectionB);
+        intersectionO.get(intersectionA).makeInvisible();
+        intersectionO.get(intersectionB).makeInvisible();
+        rect.makeVisible();
+        intersectionO.get(intersectionA).makeVisible();
+        intersectionO.get(intersectionB).makeVisible();
+    }
+    
+    /**
+     * Resize the rectangle to the correct dimensions to the road
+     */
+    private void resize(Rectangle rect, String intersectionA, String intersectionB){
+        int x = (intersectionP.get(intersectionA)[0] - intersectionP.get(intersectionB)[0]);
+        int xsqr = x*x;
+        int y = (intersectionP.get(intersectionA)[1] - intersectionP.get(intersectionB)[1]);
+        int ysqr = y*y; 
+        double hipotenuse = Math.sqrt(xsqr + ysqr);
+        int size = (int) Math.round(hipotenuse);
+        rect.changeSize(10, size);
+    }
+    
+    /**
+     * rotate the rectangle for connect with the intersectionB
+     */
+    private void rotate(Rectangle rect, String intersectionA, String intersectionB){
+        int x = (intersectionP.get(intersectionA)[0] - intersectionP.get(intersectionB)[0]);
+        int xsqr = x*x;
+        int y = (intersectionP.get(intersectionA)[1] - intersectionP.get(intersectionB)[1]);
+        int ysqr = y*y; 
+        double hipotenuse = Math.sqrt(xsqr + ysqr);
+        if(!(intersectionP.get(intersectionA)[1] == intersectionP.get(intersectionB)[1])){                  //If the intersections are in the same y axis, then not rotate
+                double theta = Math.asin(x/hipotenuse);
+                rect.rotate(theta);
+            }
+        if(intersectionP.get(intersectionA)[0] == intersectionP.get(intersectionB)[0]){        //If the intersections are in the same x axis, then rotate 90 degrees
+                int size = (int) Math.round(hipotenuse);
+                System.out.println("Hi");
+                rect.changeSize(size, 10);
+                rect.moveHorizontal(-5);
+        }
+    }
+    
+    /**
+     * Put a sign in the canvas of the simulation
+     * @param intersectionA and intersectionB indicates the road that going to have the sign
+     * @param speedLimit indicates the value of the sign
+     */
+    public void putSign(String intersectionA, String intersectionB, int speedLimit){
+        String[] touple = new String[2];
+        touple[0] = intersectionA;
+        touple[1] = intersectionB;
+        String[] toupleR = reverseTouple(touple);
+        if(isIn(signsC, touple, toupleR) == false && isIn(routesC, touple, toupleR) == true){
+            signsC.add(touple);
+            createSign(intersectionA, intersectionB, speedLimit, touple);            
+            ok = true;
+        }else{
+            JOptionPane.showMessageDialog(null, "This sign already exists or the route not exists");
+            ok = false;
+        }
+    }
+    
+    /**
+     * Create the sign in the canvas
+     */
+    private void createSign(String intersectionA, String intersectionB, int speedLimit, String[] touple){
+        String speed = String.valueOf(speedLimit);
+        Sign sign = new Sign(speed);
+        signsO.put(touple, sign);
+        moveSign(sign, intersectionA);
+    }
+    
+    /**
+     * Move the signal over the intersection A
+     */
+    private void moveSign(Sign sign, String intersectionA){
+        int x = intersectionP.get(intersectionA)[0];
+        int y = intersectionP.get(intersectionA)[1];
+        int moveX = x + 30;
+        int moveY = y - 10;
+        sign.move(moveX, moveY);
+    }
+    
+    /**
+     * Delete an intersection from the simulation
+     * @param color indicates the intersection that wants delete
+     */
+    public void delIntersection(String color){
+        if(intersectionC.contains(color)){
+            intersectionC.remove(color);
+            intersectionO.get(color).makeInvisible();
+            intersectionP.remove(color);
+            intersectionO.remove(color);
+            ok = true;
+        }else{
+            JOptionPane.showMessageDialog(null, "This intersection not exists");
+            ok = false;
+        }
+    }
+    
+    /**
+     * Delete a road from the simulation
+     * @param intersectionA and intersectionB indicates the road that wants delete
+     */
+    public void delRoad(String locationA, String locationB){
+        String[] touple = new String[2];
+        touple[0] = locationA;
+        touple[1] = locationB;
+        String[] toupleR = reverseTouple(touple);
+        if(isIn(routesC, touple, toupleR)){
+            removeSign(locationA, locationB);
+            routesC.remove(touple);
+            String[] key = findKeyR(touple, routesO);
+            routesO.get(key).makeInvisible();
+            routesO.remove(key);
+            ok = true;
+        }else{
+            JOptionPane.showMessageDialog(null, "This road not exists");
+            ok = false;
+        }
+    }
+    
+    /**
+     * Remove a specific sign from the canvas of the simulation
+     * @param intersectionA and intersectionB indicates the road that going to have the sign
+     */
+    public void removeSign(String intersectionA, String intersectionB){
+        String[] touple = new String[2];
+        touple[0] = intersectionA;
+        touple[1] = intersectionB;
+        String[] toupleR = reverseTouple(touple);
+        if(isIn(signsC, touple, toupleR)){
+            signsC.remove(touple);
+            String[] key = findKeyS(touple, signsO);
+            signsO.get(key).makeInvisible();
+            signsO.remove(key);
+            ok = true;
+        }else{
+            JOptionPane.showMessageDialog(null, "This sign not exists");
+            ok = false;
+        }     
+    }
+    
+    /**
+     * return if the last method was successfully finished 
+     */
+    public boolean ok(){
+        return ok;
+    }
+    
+    /**
+     * return the collection of the intersections in the simulation
+     */
+    public String[] intersections(){
+        String[] intersections = new String[intersectionC.size()];
+        for(int i = 0; i < intersectionC.size(); i++){
+            intersections[i] = intersectionC.get(i);
+        }
+        return intersections;
+    }
+    
+    /**
+     * return the collection of the routes in the simulation
+     */
+    public String[][] roads(){
+        String[][] roads = new String[routesC.size()][];
+        for(int i = 0; i < routesC.size(); i++){
+            roads[i] = routesC.get(i);
+        }
+        return roads;
+    }
+    
+    /**
+     * Makes visible the canvas with the simulation
+     */
+    public void makeVisible(){
+        if(!isVisible){
+            for(String[] key: routesO.keySet()){
+                routesO.get(key).makeVisible();
+            }
+            for(String key: intersectionO.keySet()){
+                intersectionO.get(key).makeVisible();
+            }
+            for(String[] key: signsO.keySet()){
+                signsO.get(key).move(0,0);
+            }
+            isVisible = true;
+        }
+    }
+    
+    /**
+     * Makes invisible the canvas with the simulation
+     */
+    public void makeInvisible(){
+        if(isVisible){
+            for(String key: intersectionO.keySet()){
+                intersectionO.get(key).makeInvisible();
+            }
+            for(String[] key: routesO.keySet()){
+                routesO.get(key).makeInvisible();
+            }
+            for(String[] key: signsO.keySet()){
+                signsO.get(key).makeInvisible();
+            }
+            isVisible = false;
+        }
+        
+    }
+    
+    /**
+     * Return if the touples are contains in the Arrays
+     */
+    private boolean isIn(ArrayList<String[]> x, String[] touple, String[] toupleR){
+        boolean flag = false;
+        for(int i = 0; i < x.size() && flag == false; i++){
+                flag = Arrays.equals(x.get(i), touple) ||  Arrays.equals(x.get(i), toupleR);
+        } 
+        return flag;
+    }
+    
+    /**
+     * Return if an intersection is hava an associated road
+     */
+    private boolean isInRoute(String color){
+        boolean flag = false;
+        for(int i = 0; i < routesC.size() && flag == false; i++){
+            flag = Arrays.asList(routesC.get(i)).contains(color);
+        }
+        return flag;
+    }
+    /**
+     * Return the reverse of a touple
+     */
+    private String[] reverseTouple(String[] touple){
+        String[] reverse = new String[2];
+        reverse[0] = touple[1];
+        reverse[1] = touple[0];
+        return reverse;
+    }
+    
+    /**
+     * Find the key of the HashMap that is equals to a parameter
+     * @param a touple to compare
+     */
+    private String[] findKeyR(String[] touple, HashMap<String[], Rectangle> hash){
+        for(String[] key : hash.keySet()){
+            if(Arrays.equals(key, touple)){
+                return key;
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Find the key of the HashMap that is equals to a parameter
+     * @param a touple to compare
+     */
+    private String[] findKeyS(String[] touple, HashMap<String[], Sign> hash){
+        for(String[] key : hash.keySet()){
+            if(Arrays.equals(key, touple)){
+                return key;
+            }
+        }
+        return null;
+    }
+}
---
+++ Csa/Sign.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.awt.Graphics2D;
+
+/**
+ * Write a description of class sing here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Sign
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<Circle> circulos = new ArrayList();
+    private String speed;
+    private boolean visible = false;
+    /**
+     * This is the constructor for the class sign where i only need the graphics with the speed
+     */
+    public Sign(String speed){
+        this.speed = speed;
+        drawSign();
+    }
+    /**
+     * This method initialize the circles that make the sign but no draw 
+     */
+    private void drawSign(){
+        Circle redPart = new Circle();
+        redPart.changeSize(25);
+        Circle whitePart = new Circle();
+        redPart.changeColor("red");
+        whitePart.changeColor("white");
+        whitePart.changeSize(redPart.getDiameter()-4);
+        whitePart.moveHorizontal(2);
+        whitePart.moveVertical(2);
+        circulos.add(redPart);
+        circulos.add(whitePart);
+    }
+    /**
+     * This method moves the entire sign into a new x and y coordenade 
+     */
+    public void move(int x , int y){
+        erase();
+        for(int i = 0 ; i < circulos.size();i++){
+            circulos.get(i).moveHorizontal(x);
+            circulos.get(i).moveVertical(y);
+            circulos.get(i).makeVisible();
+        }
+        Canvas canvas =  Canvas.getCanvas();
+        canvas.drwString(speed,circulos.get(1).getX()+6,circulos.get(1).getY()+15);
+    }
+    public void makeInvisible(){
+        for(int i = 0; i < circulos.size();i++){
+            circulos.get(i).makeInvisible();
+        }
+        erase();
+    }
+    /**
+     * To delete de canvas
+     */
+    private void erase(){
+        Canvas canvas = Canvas.getCanvas();
+        canvas.erase(this);
+        
+    }
+    
+    
+}
